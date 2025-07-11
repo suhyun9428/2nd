ef707cf22432c990f2d5b5d51ff3ddab5513ebbb
김수현 <hvl1789@gepplus.net>
2025-07-11 17:02:17 +0900

    createPortaling

--- src/components/ToastLottie/ToastLottie.jsx
+++ src/components/ToastLottie/ToastLottie.jsx
@@ -2,6 +2,7 @@ import { useRef, useState } from "react";
 import { createPortal } from "react-dom";
 import AddCounter from "../fragment/AddCounter";
 import "../../css/toastlottie.css";
+import ModalPortal from "../fragment/ModalPortal";
 
 const ToastLottie = () => {
   const toastRef = useRef(null);
@@ -15,14 +16,18 @@ const ToastLottie = () => {
 
   return (
     <div className="box__toaselottie">
-      <button type="button" className="button__toast" onClick={handleToast}>
-        토스트 뿅
-      </button>
-      <div className="box__toast" ref={toastRef}>
-        <span className="text">토스트 팝업입니다.</span>
+      <div>
+        <button type="button" className="button__toast" onClick={handleToast}>
+          토스트 뿅
+        </button>
+        {/* <ModalPortal> */}
+          <div className="box__toast" ref={toastRef}>
+            <span className="text">토스트 팝업입니다.</span>
+          </div>
+        {/* </ModalPortal> */}
+        <AddCounter />
+        {/* lottie 용인지 props로 넘기고 */}
       </div>
-      <AddCounter />
-      {/* lottie 용인지 props로 넘기고 */}
     </div>
   );
 };
---
+++ src/components/fragment/ModalPortal.jsx
@@ -0,0 +1,7 @@
+import { createPortal } from "react-dom";
+
+const ModalPortal = ({children}) =>{
+    const modalRoot = document.getElementById('modal-root');
+    return createPortal(children, modalRoot);
+}
+export default ModalPortal;
